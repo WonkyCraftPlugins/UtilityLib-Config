fc2bbb6ace92c20d94b9baba2c47f3466dd21977
Wonkglorg <Wonkglorg+github@protonmail.com>
2026-02-16 23:20:22 +0100

    Allows LangRequests to process lists of files.

--- src/main/java/com/wonkglorg/utilitylib/config/lang/LangRequest.java
+++ src/main/java/com/wonkglorg/utilitylib/config/lang/LangRequest.java
@@ -7,8 +7,8 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -53,11 +53,11 @@ public class LangRequest{
 	/**
 	 * The initial returned result without any other modifications
 	 */
-	private final String initialResult;
+	private final List<String> initialResult;
 	/**
 	 * The current result made after all values are collected
 	 */
-	private String result;
+	private List<String> result;
 	/**
 	 * Pattern for component replacers
 	 */
@@ -88,7 +88,11 @@ public class LangRequest{
 	public LangRequest replace(String value, String replacement) {
 		if(replacement == null) return this;
 		replacements.put(value, replacement);
-		result = result.replace(value, replacement);
+		for(int i = 0; i < result.size(); i++){
+			String input = result.get(i);
+			input = input.replace(value, replacement);
+			result.set(i, input);
+		}
 		return this;
 	}
 	
@@ -141,7 +145,7 @@ public class LangRequest{
 		return this;
 	}
 	
-	public String getUnmodifiedResult() {
+	public List<String> getUnmodifiedResult() {
 		return initialResult;
 	}
 	
@@ -152,50 +156,65 @@ public class LangRequest{
 	/**
 	 * Updates the input with all replacements found in the populated replacement map
 	 *
-	 * @param input to modify
+	 * @param inputs to modify
 	 * @return modified input
 	 */
-	private String updateResult(String input) {
-		for(var replacement : replacements.entrySet()){
-			input = input.replace(replacement.getKey(), replacement.getValue());
+	private List<String> updateResult(List<String> inputs) {
+		
+		for(int i = 0; i < inputs.size(); i++){
+			String input = inputs.get(i);
+			for(var replacement : replacements.entrySet()){
+				input = input.replace(replacement.getKey(), replacement.getValue());
+			}
+			inputs.set(i, input);
 		}
-		return input;
+		
+		return inputs;
 	}
 	
-	public String getResult() {
+	public List<String> getResult() {
 		return result;
 	}
 	
-	private Component toComponent(Function<String, Component> toComponent, String result) {
+	private List<Component> toComponent(Function<String, Component> toComponent, List<String> results) {
+		List<Component> components = new ArrayList<>();
 		if(componentReplacements.isEmpty()){
-			return toComponent.apply(result);
+			for(var resultValue : results){
+				components.add(toComponent.apply(resultValue));
+			}
+			
+			return components;
 		}
 		
 		if(pattern == null){
 			pattern = Pattern.compile(componentReplacements.keySet().stream().map(Pattern::quote).collect(Collectors.joining("|")));
 		}
-		
-		Matcher matcher = pattern.matcher(result);
-		
-		List<Component> components = new ArrayList<>();
-		int last = 0;
-		
-		while(matcher.find()){
-			if(matcher.start() > last){
-				components.add(toComponent.apply(result.substring(last, matcher.start())));
+		for(var resultValue : results){
+			Matcher matcher = pattern.matcher(resultValue);
+			
+			List<Component> subComponents = new ArrayList<>();
+			int last = 0;
+			
+			while(matcher.find()){
+				if(matcher.start() > last){
+					subComponents.add(toComponent.apply(resultValue.substring(last, matcher.start())));
+				}
+				
+				String key = matcher.group();
+				subComponents.add(componentReplacements.getOrDefault(key, toComponent.apply(key)));
+				
+				last = matcher.end();
 			}
 			
-			String key = matcher.group();
-			components.add(componentReplacements.getOrDefault(key, toComponent.apply(key)));
+			if(last < resultValue.length()){
+				subComponents.add(toComponent.apply(resultValue.substring(last)));
+			}
 			
-			last = matcher.end();
+			components.add(Component.join(JoinConfiguration.noSeparators(), subComponents));
+			
 		}
 		
-		if(last < result.length()){
-			components.add(toComponent.apply(result.substring(last)));
-		}
-		
-		return Component.join(JoinConfiguration.noSeparators(), components);
+		return components;
 	}
 	
 	/**
@@ -203,14 +222,14 @@ public class LangRequest{
 	 * @param toComponent the converter to use
 	 * @return the output as a component
 	 */
-	public Component toComponent(Function<String, Component> toComponent) {
+	public List<Component> toComponent(Function<String, Component> toComponent) {
 		return toComponent(toComponent, result);
 	}
 	
 	/**
 	 * @return the output as a component
 	 */
-	public Component toComponent() {
+	public List<Component> toComponent() {
 		return toComponent(MiniMessage.miniMessage()::deserialize, result);
 	}
 	
@@ -231,14 +250,14 @@ public class LangRequest{
 	 */
 	public void sendToAudience(@NotNull Audience audience, Function<String, Component> toComponent) {
 		if(audience instanceof Player player){
-			if(player.locale() == locale || forceLocale){
-				audience.sendMessage(toComponent(toComponent, this.result));
+			if(player.locale() == locale || forceLocale || langManager.getAllLangs().size() == 1){
+				toComponent(toComponent, this.result).forEach(audience::sendMessage);
 			} else {
 				this.result = updateResult(getValue(this.locale, key, defaultValue));
-				audience.sendMessage(toComponent(toComponent, this.result));
+				toComponent(toComponent, this.result).forEach(audience::sendMessage);
 			}
 		} else {
-			audience.sendMessage(toComponent(toComponent, this.result));
+			toComponent(toComponent, this.result).forEach(audience::sendMessage);
 		}
 	}
 	
@@ -250,8 +269,7 @@ public class LangRequest{
 	 * @param defaultValue the default value to return if no value was found
 	 * @return the returned result or the value if no result was found
 	 */
-	@Contract(pure = true, value = "_,null,null -> null; _,_,!null -> !null")
-	private String getValue(final Locale locale, final String key, String defaultValue) {
+	private List<String> getValue(@Nullable final Locale locale, @NotNull final String key, @Nullable String defaultValue) {
 		LangConfig config;
 		defaultValue = defaultValue != null ? defaultValue : key;
 		var configOptional = langManager.getAnyValidLangConfig(locale);
@@ -259,27 +277,51 @@ public class LangRequest{
 			config = configOptional.get();
 		} else {
 			logger.log(Level.INFO, "No lang file could be loaded for request: " + key + " using default value!");
-			return defaultValue;
-		}
-		
-		String editString = config.getString(key);
-		if(editString == null){
-			editString = defaultValue;
-		}
-		
-		for(var mapValue : langManager.getReplacerMap().entrySet()){
-			editString = editString.replace(mapValue.getKey(), mapValue.getValue());
+			return List.of(defaultValue);
 		}
 		
 		if(config.isUpdateRequest()){
 			config.updateReplacerMap();
 		}
 		
-		for(var mapValue : config.getReplacerMap().entrySet()){
-			editString = editString.replace(mapValue.getKey(), mapValue.getValue());
+		if(config.isList(key)){
+			List<String> results = config.getStringList(key);
+			
+			for(int i = 0; i < results.size(); i++){
+				String edited = results.get(i);
+				
+				for(var entry : langManager.getReplacerMap().entrySet()){
+					edited = edited.replace(entry.getKey(), entry.getValue());
+				}
+				
+				for(var entry : config.getReplacerMap().entrySet()){
+					edited = edited.replace(entry.getKey(), entry.getValue());
+				}
+				
+				results.set(i, edited);
+			}
+			
+			return results;
+			
+		} else {
+			String resultString = config.getString(key, defaultValue);
+			if(langManager.getReplacerMap().isEmpty() && config.getReplacerMap().isEmpty()){
+				List<String> strings = new ArrayList<>();
+				strings.add(resultString);
+				return strings;
+			}
+			
+			for(var entry : langManager.getReplacerMap().entrySet()){
+				resultString = resultString.replace(entry.getKey(), entry.getValue());
+			}
+			
+			for(var entry : config.getReplacerMap().entrySet()){
+				resultString = resultString.replace(entry.getKey(), entry.getValue());
+			}
+			List<String> strings = new ArrayList<>();
+			strings.add(resultString);
+			return strings;
 		}
-		
-		return editString;
 	}
 	
 }
